84f6d4ca346d6a0defe56e3fa7198614adf71ecf
Arthurito <Arthuriito>
2023-08-30 08:48:05 -0300

    novas testemunhas hihihihi

--- js/main.js
+++ js/main.js
@@ -130,23 +130,23 @@
 
 
     // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
         center: true,
-        autoplay: true,
-        dots: true,
+        dots: false,
         loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
         responsive: {
             0:{
-                items:1
-            },
-            576:{
-                items:1
+                items: 1
             },
             768:{
-                items:2
-            },
-            992:{
-                items:3
+                items: 2
             }
         }
     });
